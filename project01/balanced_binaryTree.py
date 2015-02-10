8a22b14c6ae964d9a440a3fdfb7df2366188bf37
Nicholas Elliot <nbelliot93@gmail.com>
2015-02-10 01:48:34 -0500

    update...
    in progress...

--- project01/balanced_binaryTree.py
+++ project01/balanced_binaryTree.py
@@ -1,21 +1,10 @@
-"""
-"Binary Tree" - balanced_binaryTree.py (5/14/14)
-Nicholas Elliot
-
-Associated Files:
-balanced_binaryTree.py
-a_star.py
-DFS.py
-music_generator.py
-"""
-
-
 class Node:    
     def __init__(self, data=""):
         self.left = None
         self.right = None
         self.parent = None
         self.data = data
+        #self.height = 0
 
 
 class BalBTree:
@@ -27,26 +16,34 @@ class BalBTree:
         return Node(data)
 
 
-    def insert(self, root, data=""):    
+    def insert(self, root, data=""):
         if root.left == None:
             root.left = self.addNode(data)
             root.left.parent = root
+            self.determine_height(root.left)
         elif root.right == None:
             root.right = self.addNode(data)
             root.right.parent = root
+            self.determine_height(root.right)
         elif self.size(root.left) > self.size(root.right):
             size = self.size(root.left)
             maxsize = self.maxSize(root.left)
             if (size == maxsize):
                 self.insert(root.right, data)
                 root.right.parent = root
+                self.determine_height(root.right)
             else:
                 self.insert(root.left, data)
                 root.left.parent = root
+                self.determine_height(root.left)
         else:
             self.insert(root.left, data)
             root.left.parent = root
-
+            self.determine_height(root.left)
+        
+        self.determine_height(self.root)
+        self.balance()
+        
 
     def maxDepth(self, root):
         if root == None:
@@ -64,7 +61,7 @@ class BalBTree:
             return self.size(root.left) + 1 + self.size(root.right)
         
         
-    def maxSize(self,root):
+    def maxSize(self, root):
         height = self.maxDepth(root)
         total = 0
         if root == None:
@@ -74,7 +71,76 @@ class BalBTree:
                 total += 2**i
                 i += 1
         return total
+    
+    
+    def determine_height(self, node):
+        count_l = (self.determine_height(node.left) + 1 if node.left != None else 0)
+        count_r = (self.determine_height(node.right) + 1 if node.right != None else 0)
+        
+        height = (count_l if count_l > count_r else count_r)
+        
+        return height
+    
+    
+    def move_left(self, node):
+        _r = node.right
+        _rl = _r.left
+        
+        node.right = _rl
+        _r.left = node
+        
+        self.determine_height(node)
+        self.determine_height(_r)
+        
+        return _r
+  
+    def move_right(self, node):
+        _l = node.left
+        _lr = _l.right
+        
+        node.left = _lr
+        _l.right = node
+        
+        self.determine_height(node)
+        self.determine_height(_l)
+        
+        return _l
 
+    def rotate_left(self, node):
+        _r = node.right
+        
+        _lH = self.determine_height(_r) if _r.left != None else 0
+        _rH = self.determine_height(_r) if _r.right != None else 0
+        
+        if _lH > _rH: node.right = _r.move_right()
+        
+        return self.move_left(node)
+
+    def rotate_right(self, node):
+        _l = node.left
+        
+        _lH = self.determine_height(_l) if _l.left != None else 0
+        _rH = self.determine_height(_l) if _l.right != None else 0
+        
+        if _rH > _lH: node.left = _l.move_left()
+        
+        return self.move_right(node)
+
+    def balance(self):
+        root = self.root
+        if root.left == None and root.right == None: return root
+       
+        _lH = self.determine_height(root.left) if root.left != None else 0
+        _rH = self.determine_height(root.right) if root.right != None else 0
+       
+        if _lH + 1 < _rH:
+            return self.rotate_left(root)
+        elif _rH + 1 < _lH:
+            return self.rotate_right(root)
+       
+        self.determine_height(root)
+        return root
+          
 
     def printTree(self, root):
         if root == None:
--- project01/rope.py
+++ project01/rope.py
@@ -4,21 +4,21 @@ class Rope:
     def __init__(self, string=""):
         self.tree = BalBTree()
         self.SIZE_FRAG = 5
-        i = 0
-        while i < len(string):
-            if len(string) - i < self.SIZE_FRAG:
-                if self.tree.root.left == None or self.tree.root.right == None:
-                    self.tree.insert(self.tree.root, string[i:i+(len(string))])
-                else:
-                    self.concat(Rope(string[i:i+(len(string))]))
-            else:
-                if self.tree.root.left == None or self.tree.root.right == None:
-                    self.tree.insert(self.tree.root, string[i:i+self.SIZE_FRAG])
-                else:
-                    self.concat(Rope(string[i:i+(len(string)-1)]))
-            i += self.SIZE_FRAG
+        if string != "": self.feed(string)
+            
         
-    #def feed(self, string):
+    def feed(self, string):
+        if len(string) < self.SIZE_FRAG:
+            if self.tree.root.left == None or self.tree.root.right == None:
+                self.tree.insert(self.tree.root, string[0:(len(string))])
+            else:
+                self.concat(Rope(string))
+        else:
+            if self.tree.root.left == None or self.tree.root.right == None:
+                self.tree.insert(self.tree.root, string[0:self.SIZE_FRAG])
+                self.feed(string[self.SIZE_FRAG:len(string)])
+            else:
+                self.concat(Rope(string))
 
             
     def weight(self, root):
@@ -27,8 +27,9 @@ class Rope:
         else:
             return self.weight(root.left) + len(root.data) +self.weight(root.right)
         
+        
     def index(self, root, i):
-        if self.weight(root) < i:
+        if self.weight(root) <= i:
             return self.index(root.right, i - self.weight(root))
         else:
             if root.left != None:
@@ -36,14 +37,36 @@ class Rope:
             else:
                 return root.data[i]
             
+            
     def find(self, root, i):
-        if self.weight(root) < i:
+        if self.weight(root) <= i:
             return self.find(root.right, i - self.weight(root))
         else:
             if root.left != None:
                 return self.find(root.left, i)
             else:
                 return root, i
+    
+    
+    def cut(self, root, i, R):
+        if self.weight(root) <= i:
+            return self.cut(root.right, i - self.weight(root), R)
+        else:
+            if root.left != None:
+                if (R.tree.root.left or R.tree.root.right == None):
+                    R.tree.root = root.right
+                    R.tree.root.left.parent = None
+                    root.right = None
+                    return self.cut(root.left, i, R)
+                else:
+                    S = Rope()
+                    S.tree.root = root.right
+                    S.tree.root.parent = None
+                    root.right = None
+                    S.concat(R)
+                    return self.cut(root.left, i, S)
+            else:
+                return R
     
     def concat(self, S):
         root = self.tree.addNode()
@@ -57,11 +80,11 @@ class Rope:
         root1.parent = root
         root2.parent = root
         self.tree.root = root
+        
+        self.tree.determine_height(root)
+        #self.tree.balance()
 
-    #def split(self, i):
-        #
-        #if i < weight 
-        """
+    def split(self, i):
         tree = self.tree
         node, index = self.find(tree.root, i)
         if index != 0:
@@ -73,23 +96,23 @@ class Rope:
                 parent.left = root
             else:
                 parent.right = root
-                
-                """
-        def insert(self, i, S):
-            node, index = self.find(self.tree.root, i)
-            root = tree.addNode()
-            root.left = node
-            node.parent = root
-            root.right = S.root
-            S.root.parent = root        
+        return self.cut(tree.root, i, Rope())
+    
+    def insert(self, i, S):
+        node, index = self.find(self.tree.root, i)
+        root = tree.addNode()
+        root.left = node
+        node.parent = root
+        root.right = S.root
+        S.root.parent = root
 
 
 if __name__ == "__main__":
-    R1 = Rope("Hello there")
+    R1 = Rope("He12o there")
     R1.tree.printTree(R1.tree.root)
     print
 
-    R2 = Rope("every one.")
+    R2 = Rope("every one that is here.")
     R2.tree.printTree(R2.tree.root)
     print
     
@@ -101,10 +124,20 @@ if __name__ == "__main__":
     
     #print R1.split(3)
     
-    R3 = Rope("123456789098")
+    R3 = Rope("Hello there my friends I love you so much")
     R3.tree.printTree(R3.tree.root)
+    print
+    R3.tree.balance()
+    R3.tree.printTree(R3.tree.root)
+    print
     
     print R1.weight(R1.tree.root)
     print
     print R1.index(R1.tree.root, 3)
+    print
     
+    #R4 = R1.split(3)
+    
+    R1.tree.printTree(R1.tree.root)
+    print
+    #R4.tree.printTree(R4.tree.root)
