b40104aa2db331eb4dce0511080f92b35b2865f4
Nicholas Elliot <nbelliot93@gmail.com>
2015-03-11 00:21:06 -0400

    tree - generate
    completely scratch "insert" method.
    
    create new method: "generate" which generates a tree with n nodes and k
    branches, via breadth-first search.

--- project02/tree.py
+++ project02/tree.py
@@ -1,4 +1,4 @@
-K = 3
+import random
 
 class Node:    
     def __init__(self, data=None, heuristic=0):
@@ -9,88 +9,61 @@ class Node:
 
 
 class Tree:
-    def __init__(self, node):
-        self.root = node
+    def __init__(self):
+        self.root = None
     
     
     def addNode(self, data=None, heuristic=0):
         return Node(data, heuristic)
 
-
-    def insert(self, root, nodes):
-        if root.children == None:
-            root.children = nodes
-            self.set_parents(root, root.children)
-        else:
-            pre_child = root.children[0]
-            pre_size = self.size(pre_child)
-            for child in root.children:
-                cur_size = self.size(child)
-                max_size = self.maxSize(child)
-                if cur_size != max_size:
-                    return self.insert(child, nodes)
-            return self.insert(root.children[0], nodes)
-
     
-    def set_parents(self, root, nodes):
-        for node in nodes:
-            node.parent = root        
+    def generate (self, total, branches):
+        count = 0
+        self.root = self.addNode("S", random.randint(0,9))
+        thislevel = [self.root]
+        count += 1
+        while count < total:
+            nextlevel = list()
+            for node in thislevel:
+                nodes = list()
+                for i in range(0, branches):
+                    temp = self.addNode(chr(ord("A") + (count -1)) + ver, random.randint(0,9))
+                    nextlevel.append(temp)
+                    nodes.append(temp)
+                    count += 1
+                self.set_relatives(node, nodes)
+            thislevel = nextlevel     
     
     
-    def height(self, root):
-        if root == None:
-            return 0
-        elif root.children == None:
-            return 1
-        else:
-            deep = 0
-            for child in root.children:
-                cur = self.height(child)
-                deep = max(deep, cur)
-            return deep + 1
+    def set_relatives(self, parent, children):
+        self.set_children(parent, children)
+        self.set_parents(parent, children)        
     
     
-    def size(self, root):
-        if root == None:
-            return 0
-        elif root.children == None:
-            return self.size(root.children) + 1
-        else:
-            total = 0
-            for child in root.children:
-                total += self.size(child) + 1
-            return total - 2
+    def set_children(self, parent, children):
+        parent.children = children
+    
+    
+    def set_parents(self, parent, children):
+        for node in children:
+            node.parent = parent        
             
-        
-    def maxSize(self,root):
-        height = self.height(root)
-        total = 1
-        if root == None:
-            return total
-        elif root.children == None:
-            return self.maxSize(root.children)
-        else:
-            for i in range(0, height):
-                total += K**i
-                i += 1
-        return total - 1
-        
     
-    def printTree(self, root):
-        thislevel = ["|", root, "|"]
+    def printTree(self):
+        thislevel = ["|", self.root, "|"]
         while thislevel:
             string = ""
             nextlevel = list()
             for node in thislevel:
                 if isinstance(node, Node):
                     string += "(" + str(node.data) + ", " + str(node.heuristic) + ")"
-                    nextlevel.append("|")
+                    #nextlevel.append("|")
                     if node.children != None:
-                        #nextlevel.append("|")
+                        nextlevel.append("|")
                         for child in node.children:
                             nextlevel.append(child)
-                        #nextlevel.append("| ")
-                    nextlevel.append("| ")
+                        nextlevel.append("| ")
+                    #nextlevel.append("| ")
                 else:
                     string += node
             print string
@@ -98,35 +71,7 @@ class Tree:
 
 
 if __name__ == "__main__":
-    T = Tree(Node("S", 11))
-    
-    print T.height(T.root)
-    print T.size(T.root)
-    print T.maxSize(T.root)
-    print
-    
-    T.insert(T.root, [T.addNode("A", 10), T.addNode("B", 9), T.addNode("C", 8)])
-    
-    print T.height(T.root)
-    print T.size(T.root)
-    print T.maxSize(T.root)        
-    
-    
-    T.insert(T.root, [T.addNode("D", 7), T.addNode("E", 6), T.addNode("F", 5)])
-    T.printTree(T.root)
-    T.insert(T.root, [T.addNode("G", 4), T.addNode("H", 3), T.addNode("I", 2)])
-    #T.insert(T.root, [T.addNode("J", 1), T.addNode("K", 0), T.addNode("L", 1)])
-    #T.insert(T.root, [T.addNode("M", 2), T.addNode("N", 3), T.addNode("0", 4)])
-    #T. printTree(T.root)
-    
-    
-    #T.insert(T.root, [T.addNode("P", 10), T.addNode("Q", 9), T.addNode("R", 8)])
-    #T.insert(T.root, [T.addNode("S", 7), T.addNode("T", 6), T.addNode("U", 5)])
-    #T.insert(T.root, [T.addNode("V", 4), T.addNode("W", 3), T.addNode("X", 2)])
-    #T.insert(T.root, [T.addNode("Y", 1), T.addNode("Z", 0)]) 
-    T.printTree(T.root)
-    
-    print T.height(T.root)
-    print T.size(T.root)
-    print T.maxSize(T.root)
+    T = Tree()
+    T.generate(26,3)
+    T.printTree()
     
